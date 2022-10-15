36fc4618943730b022b2912bbe5482eb3540a704
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-10-15 21:49:00 +0900

    fix init/create_table.sh

--- init/create_table.sh
+++ init/create_table.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
-$CMD_MYSQL -e "create table person (
-    id int(10)  AUTO_INCREMENT NOT NULL primary key,
-    name varchar(50) NOT NULL
-    );"
-$CMD_MYSQL -e  "insert into person values (1, 'shin');"
-$CMD_MYSQL -e  "insert into article values (2, 'yamaga');"
+# CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
+# $CMD_MYSQL -e "create table person (
+#     id int(10)  AUTO_INCREMENT NOT NULL primary key,
+#     name varchar(50) NOT NULL
+#     );"
+# $CMD_MYSQL -e  "insert into person values (1, 'shin');"
+# $CMD_MYSQL -e  "insert into article values (2, 'yamaga');"
